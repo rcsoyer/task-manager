ac0fb932ec6635ef119a467674b883afaa95ddf8
rafasoyer <r.soyer@carepay.com>
2021-08-08 15:54:04 +0200

    modeling

--- src/main/java/org/acme/taskmanager/domain/Process.java
+++ src/main/java/org/acme/taskmanager/domain/Process.java
@@ -3,4 +3,11 @@ package org.acme.taskmanager.domain;
 import java.util.UUID;
 
 public record Process(UUID pid, Priority priority) {
+
+    /**
+     * Terminate a process
+     */
+    void kill() {
+
+    }
 }
