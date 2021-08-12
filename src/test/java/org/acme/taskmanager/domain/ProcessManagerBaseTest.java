ea8caaebcca8ffe78b29bccb37319818300e7ec3
rafasoyer <r.soyer@carepay.com>
2021-08-12 21:34:57 +0200

    more tests

--- src/test/java/org/acme/taskmanager/domain/ProcessManagerBaseTest.java
+++ src/test/java/org/acme/taskmanager/domain/ProcessManagerBaseTest.java
@@ -3,7 +3,7 @@ package org.acme.taskmanager.domain;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.List;
-import java.util.UUID;
+import java.util.Queue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
@@ -12,20 +12,27 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import static java.util.Comparator.comparing;
+import static java.util.UUID.randomUUID;
+import static org.acme.taskmanager.domain.Priority.HIGH;
+import static org.acme.taskmanager.domain.Priority.LOW;
+import static org.acme.taskmanager.domain.Priority.MEDIUM;
 import static org.acme.taskmanager.domain.SortProcesses.PID;
+import static org.acme.taskmanager.domain.SortProcesses.TIME;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class ProcessManagerBaseTest {
 
     private ProcessManager processManager;
+    private Queue<Process> processes;
 
     @BeforeAll
     void setUp() {
-        final var processes = new ArrayDeque<Process>(3);
-        processes.add(new Process(UUID.randomUUID(), Priority.HIGH));
-        processes.add(new Process(UUID.randomUUID(), Priority.MEDIUM));
-        processes.add(new Process(UUID.randomUUID(), Priority.LOW));
+        processes = new ArrayDeque<>(3);
+        processes.add(new Process(randomUUID(), HIGH));
+        processes.add(new Process(randomUUID(), MEDIUM));
+        processes.add(new Process(randomUUID(), LOW));
+
         processManager = new ProcessManagerBase(processes) {
             @Override
             public boolean addProcess(final Process process) {
@@ -39,11 +46,19 @@ class ProcessManagerBaseTest {
 
         @Test
         void listProcesses_byPID() {
-            final Collection<Process> processes = processManager.listProcesses(PID);
-            processes.forEach(process -> System.out.println("process: " + process));
+            final Collection<Process> orderedByPID = processManager.listProcesses(PID);
 
-            assertThat(List.copyOf(processes))
+            assertThat(List.copyOf(orderedByPID))
               .isSortedAccordingTo(comparing(Process::pid));
+        }
+
+        @Test
+        void listProcesses_byTime() {
+            final Collection<Process> orderedByTime = processManager.listProcesses(TIME);
+
+            assertThat(orderedByTime)
+              .isNotSameAs(processes)
+              .containsExactlyElementsOf(processes);
         }
     }
 
