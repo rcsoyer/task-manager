67dc5353260f2efa0bc03501c8afd7ebeeb1c476
rafasoyer <r.soyer@carepay.com>
2021-08-12 21:57:50 +0200

    more tests

---
+++ src/test/java/org/acme/taskmanager/domain/PriorityTest.java
@@ -0,0 +1,36 @@
+package org.acme.taskmanager.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.acme.taskmanager.domain.Priority.HIGH;
+import static org.acme.taskmanager.domain.Priority.LOW;
+import static org.acme.taskmanager.domain.Priority.MEDIUM;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PriorityTest {
+
+    @Test
+    void isHigher_Low() {
+
+        assertFalse(LOW.isHigher(HIGH));
+        assertFalse(LOW.isHigher(MEDIUM));
+        assertFalse(LOW.isHigher(LOW));
+    }
+
+    @Test
+    void isHigher_Medium() {
+
+        assertFalse(MEDIUM.isHigher(HIGH));
+        assertTrue(MEDIUM.isHigher(LOW));
+        assertFalse(MEDIUM.isHigher(MEDIUM));
+    }
+
+    @Test
+    void isHigher_High() {
+
+        assertTrue(HIGH.isHigher(LOW));
+        assertTrue(HIGH.isHigher(MEDIUM));
+        assertFalse(HIGH.isHigher(HIGH));
+    }
+}
