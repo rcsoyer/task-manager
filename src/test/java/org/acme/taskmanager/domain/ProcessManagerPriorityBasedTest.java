ccaf9c9fb36f3b158975ccaa2c00cacb270358ac
rafasoyer <r.soyer@carepay.com>
2021-08-13 18:08:12 +0200

    more tests after refactoring

--- src/test/java/org/acme/taskmanager/domain/ProcessManagerPriorityBasedTest.java
+++ src/test/java/org/acme/taskmanager/domain/ProcessManagerPriorityBasedTest.java
@@ -1,17 +1,53 @@
 package org.acme.taskmanager.domain;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.util.List.of;
+import static java.util.UUID.randomUUID;
+import static org.acme.taskmanager.domain.Priority.HIGH;
+import static org.acme.taskmanager.domain.Priority.LOW;
+import static org.acme.taskmanager.domain.Priority.MEDIUM;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ProcessManagerPriorityBasedTest {
 
+    private static final int DEFAULT_MAX_SIZE_PROCESSES = 5;
+
+    private ProcessManagerPriorityBased processManager;
+    private Queue<Process> processes;
+
     @BeforeEach
     void setUp() {
+        processManager = new ProcessManagerPriorityBased();
+        processes = new ArrayDeque<>(of(new Process(randomUUID(), HIGH),
+                                        new Process(randomUUID(), MEDIUM),
+                                        new Process(randomUUID(), LOW),
+                                        new Process(randomUUID(), HIGH),
+                                        new Process(randomUUID(), MEDIUM)));
+        processes.forEach(processManager::add);
     }
 
-    @Test
-    void add() {
+    @Nested
+    class Add {
+
+        @Test
+        void add_whenHasNoSpaceThenOldestLowestPriorityIsRemoved() {
+            final var addition = new Process(randomUUID(), MEDIUM);
+
+            assertThat(processManager.getManagedProcesses())
+              .hasSize(DEFAULT_MAX_SIZE_PROCESSES);
+
+            assertTrue(processManager.add(addition));
+
+            assertThat(processManager.getManagedProcesses())
+              .contains(addition)
+              .hasSize(DEFAULT_MAX_SIZE_PROCESSES);
+        }
     }
 }
