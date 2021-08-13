b4f0683aaf7f52f9a00286db45b4a129f967402f
rafasoyer <r.soyer@carepay.com>
2021-08-13 20:05:33 +0200

    adding utility class for validation throwing IllegalArgumentException. Surprisingly enough neither the JDK nor Java libs has an easier method provided exactly that

--- src/main/java/org/acme/taskmanager/domain/Priority.java
+++ src/main/java/org/acme/taskmanager/domain/Priority.java
@@ -1,5 +1,9 @@
 package org.acme.taskmanager.domain;
 
+/**
+ * @implNote The declaration order of the Enum values matters for the evaluation of the method {@link
+ * Priority#isHigher(Priority)}
+ */
 public enum Priority {
 
     LOW,
