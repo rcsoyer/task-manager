7514bc6d38800c550b3c23b34d06d558a4ddc895
rafasoyer <r.soyer@carepay.com>
2021-08-13 15:36:54 +0200

    refactoring

--- src/main/java/org/acme/taskmanager/domain/ProcessManagerDefault.java
+++ src/main/java/org/acme/taskmanager/domain/ProcessManagerDefault.java
@@ -8,56 +8,56 @@ import static java.util.Collections.unmodifiableCollection;
 import static java.util.Comparator.comparing;
 
 public class ProcessManagerDefault implements ProcessManager {
-    
+
     private static final int MAX_NUMBER_PROCESSES = 5;
-    
+
     private final Queue<Process> managedProcesses = new ArrayDeque<>(MAX_NUMBER_PROCESSES);
-    
+
     @Override
     public final Collection<Process> listSorted(final SortProcesses sorting) {
         return switch (sorting) {
             case TIME -> getManagedProcesses();
             case PID -> managedProcesses.stream()
-                    .sorted(comparing(Process::pid))
-                    .toList();
+                                        .sorted(comparing(Process::pid))
+                                        .toList();
             case PRIORITY -> managedProcesses.stream()
-                    .sorted(comparing(Process::priority))
-                    .toList();
+                                             .sorted(comparing(Process::priority))
+                                             .toList();
         };
     }
-    
+
     @Override
     public final boolean kill(final Process process) {
         return managedProcesses.remove(process);
     }
-    
+
     @Override
     public final boolean killAllProcessesBy(final Priority priority) {
         return managedProcesses.removeIf(process -> process.priority() == priority);
     }
-    
+
     @Override
     public final void killAllProcesses() {
         managedProcesses.clear();
     }
-    
+
     @Override
     public boolean add(final Process process) {
         if (!isCapacityReached()) {
             return managedProcesses.offer(process);
         }
-        
+
         return false;
     }
-    
+
     protected void killOldest() {
         managedProcesses.poll();
     }
-    
+
     protected boolean isCapacityReached() {
         return managedProcesses.size() == MAX_NUMBER_PROCESSES;
     }
-    
+
     protected Collection<Process> getManagedProcesses() {
         return unmodifiableCollection(managedProcesses);
     }
