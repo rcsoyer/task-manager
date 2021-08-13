57644c00676411359ec96f83111089516f63bc76
rafasoyer <r.soyer@carepay.com>
2021-08-13 19:52:27 +0200

    adding utility class for validation throwing IllegalArgumentException. Surprisingly enough neither the JDK nor Java libs has an easier method provided exactly that

--- src/main/java/org/acme/taskmanager/domain/Process.java
+++ src/main/java/org/acme/taskmanager/domain/Process.java
@@ -2,9 +2,18 @@ package org.acme.taskmanager.domain;
 
 import java.util.UUID;
 
+import static org.acme.taskmanager.domain.util.ParametersValidationUtils.requireNonNull;
+
+
 public record Process(UUID pid, Priority priority) {
 
+    public Process {
+        requireNonNull(pid, "Missing mandatory data to create a 'Process': PID");
+        requireNonNull(priority, "Missing mandatory data to create a 'Process': Priority");
+    }
+
     public boolean isHigher(final Process other) {
+        requireNonNull(other, "Required 'Process' to be checked was not passed");
         return priority.isHigher(other.priority());
     }
 }
---
+++ src/main/java/org/acme/taskmanager/domain/util/ParametersValidationUtils.java
@@ -0,0 +1,20 @@
+package org.acme.taskmanager.domain.util;
+
+import java.util.Optional;
+
+public final class ParametersValidationUtils {
+
+    private ParametersValidationUtils() {
+    }
+
+    /**
+     * Validate the given parameter is not null
+     *
+     * @return the passed parameter
+     * @throws IllegalArgumentException with the provided error message if the validated parameter object is null
+     */
+    public static <T> T requireNonNull(final T possibleNull, final String errorMessage) {
+        return Optional.ofNullable(possibleNull)
+                       .orElseThrow(() -> new IllegalArgumentException(errorMessage));
+    }
+}
