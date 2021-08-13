ac26c78be648d859a55b6003e6be033446b83e2a
rafasoyer <r.soyer@carepay.com>
2021-08-13 16:08:14 +0200

    more tests

---
+++ src/test/java/org/acme/taskmanager/domain/ProcessManagerOlderDiesFirstTest.java
@@ -0,0 +1,64 @@
+package org.acme.taskmanager.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static java.util.UUID.randomUUID;
+import static org.acme.taskmanager.domain.Priority.HIGH;
+import static org.acme.taskmanager.domain.Priority.MEDIUM;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProcessManagerOlderDiesFirstTest {
+
+    private static final int DEFAULT_MAX_SIZE_PROCESSES = 5;
+
+    private ProcessManagerOlderDiesFirst processManager;
+
+    @BeforeEach
+    void setUp() {
+        processManager = new ProcessManagerOlderDiesFirst();
+    }
+
+    @Test
+    void add_whenHasSpaceThenOldestNotRemoved() {
+        final var first = new Process(randomUUID(), HIGH);
+
+        processManager.add(first);
+
+        assertThat(processManager.getManagedProcesses())
+          .hasSizeLessThan(DEFAULT_MAX_SIZE_PROCESSES);
+
+        final var last = new Process(randomUUID(), MEDIUM);
+
+        assertTrue(processManager.add(last));
+
+        assertThat(processManager.getManagedProcesses())
+          .containsExactly(first, last);
+    }
+
+    @Test
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    void add_whenHasNoSpaceThenOldestIsRemoved() {
+        addProcessesUntilMax();
+
+        assertThat(processManager.getManagedProcesses())
+          .hasSize(DEFAULT_MAX_SIZE_PROCESSES);
+
+        final Process oldest = processManager.getManagedProcesses().stream().findFirst().get();
+        final var addition = new Process(randomUUID(), HIGH);
+
+        assertTrue(processManager.add(addition));
+
+        assertThat(processManager.getManagedProcesses())
+          .hasSize(DEFAULT_MAX_SIZE_PROCESSES)
+          .contains(addition)
+          .doesNotContain(oldest);
+    }
+
+    private void addProcessesUntilMax() {
+        while (processManager.getManagedProcesses().size() != DEFAULT_MAX_SIZE_PROCESSES) {
+            processManager.add(new Process(randomUUID(), HIGH));
+        }
+    }
+}
