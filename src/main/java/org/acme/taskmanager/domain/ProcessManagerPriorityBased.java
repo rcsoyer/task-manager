3c818c6856cf875d89223226b61b30094a487789
rafasoyer <r.soyer@carepay.com>
2021-08-13 18:02:55 +0200

    refactoring

--- src/main/java/org/acme/taskmanager/domain/ProcessManagerPriorityBased.java
+++ src/main/java/org/acme/taskmanager/domain/ProcessManagerPriorityBased.java
@@ -1,16 +1,17 @@
 package org.acme.taskmanager.domain;
 
-import static java.util.Comparator.comparing;
-
 public final class ProcessManagerPriorityBased extends ProcessManagerDefault {
 
     @Override
-    public boolean add(final Process process) {
-        getManagedProcesses()
-          .stream()
-          .min(comparing(Process::priority))
-          .filter(process::isHigher)
-          .ifPresent(this::kill);
-        return super.add(process);
+    public boolean add(final Process addition) {
+        if (isCapacityReached()) {
+            getManagedProcesses()
+              .stream()
+              .filter(addition::isHigher)
+              .findFirst()
+              .ifPresent(this::kill);
+        }
+
+        return super.add(addition);
     }
 }
