c3426be31b684482ba3c63b4e1b9d7adb1948432
rafasoyer <r.soyer@carepay.com>
2021-08-17 19:17:59 +0200

    revert. leave only the test units. CLI app would take too much work

--- src/main/java/org/acme/taskmanager/app/TaskManagerCli.java
+++
@@ -1,9 +0,0 @@
-package org.acme.taskmanager.app;
-
-public class TaskManagerCli {
-
-    public static void main(final String[] args) {
-        System.out.println("\nTask Manager app");
-        new TaskManagerControl().start();
-    }
-}
